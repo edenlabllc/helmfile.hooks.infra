48eacefb9ddc67544037cbab05fb08dba715a7da
Aliaksandr Panasiuk <apanasyuk@edenlab.com.ua>
2024-07-17 17:47:33 +0200

    #7 - refactoring

--- bin/azure-cluster-uninstall-hook.sh
+++ bin/azure-cluster-uninstall-hook.sh
@@ -4,6 +4,6 @@ set -e
 
 CLUSTER_NAME="${1}"
 NAMESPACE="${2}"
-WAIT_DELETE_CLUSTER="${3:-false}"
+WAIT_FOR_CLUSTER_DELETION="${3:-false}"
 
-kubectl --namespace "${NAMESPACE}" delete cluster "${CLUSTER_NAME}" "--wait=${WAIT_DELETE_CLUSTER}"
+kubectl --namespace "${NAMESPACE}" delete cluster "${CLUSTER_NAME}" "--wait=${WAIT_FOR_CLUSTER_DELETION}"
