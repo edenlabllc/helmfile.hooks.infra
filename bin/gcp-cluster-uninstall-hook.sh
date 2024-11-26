23a7d56dee451bf6bc72b0ffc185aefcd85605e1
Aliaksandr Panasiuk <apanasyuk@edenlab.com.ua>
2024-11-26 13:29:43 +0100

    #29 - refactoring

--- bin/gcp-cluster-uninstall-hook.sh
+++ bin/gcp-cluster-uninstall-hook.sh
@@ -8,4 +8,5 @@ WAIT_FOR_CLUSTER_DELETION="${3:-false}"
 
 kubectl --namespace "${NAMESPACE}" delete cluster "${CLUSTER_NAME}" "--wait=${WAIT_FOR_CLUSTER_DELETION}"
 
+# Required for correct release removal of gcp-cluster via Helm
 sleep 3
