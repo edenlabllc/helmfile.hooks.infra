de3b4b75824f863b3ebda10b3ac48a9751b195d5
Aliaksandr Panasiuk <apanasyuk@edenlab.com.ua>
2024-11-26 13:14:58 +0100

    #7 - Add gcp-cluster-*.sh hooks based on the AWS's ones.

---
+++ bin/gcp-cluster-uninstall-hook.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+CLUSTER_NAME="${1}"
+NAMESPACE="${2}"
+WAIT_FOR_CLUSTER_DELETION="${3:-false}"
+
+kubectl --namespace "${NAMESPACE}" delete cluster "${CLUSTER_NAME}" "--wait=${WAIT_FOR_CLUSTER_DELETION}"
+
+sleep 3
