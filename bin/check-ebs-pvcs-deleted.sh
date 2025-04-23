666888444796aca4a52aa6c1023b321673f00c1d
Vadim <vadim_bogdanovsky@yahoo.com>
2025-04-23 14:46:19 +0300

    add check-ebs-pvcs-deleted preuninstall

---
+++ bin/check-ebs-pvcs-deleted.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+LIMIT="${1:-120}"
+
+PVC_IDS=( "$(kubectl get -A pvc -o yaml | yq '.items[] | select(.metadata.annotations["volume.kubernetes.io/storage-provisioner"] == "ebs.csi.aws.com") | .spec.volumeName')" )
+
+for PVC_ID in ${PVC_IDS[*]}; do
+  COUNT=1
+  while (kubectl get pv "${PVC_ID}" &> /dev/null); do
+    if (( COUNT > LIMIT )); then
+      >2& echo "Limit exceeded."
+      exit 1
+    fi
+
+    echo "PV name: ${PVC_ID} in the process of being removed."
+    sleep 1
+    ((++COUNT))
+  done
+done
