17217a05f12dbef6e42ab2f008a296a7996d2e3b
Anton Novikov <a.novikov@edenlab.com.ua>
2025-11-03 19:06:30 +0100

    Split clickhouse hook into wait for ready & deleted.

---
+++ bin/postsync-wait-clickhouse-ready.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+RELEASE_NAME="${1:-clickhouse}"
+NAMESPACE="${2:-clickhouse}"
+LIMIT="${3:-180}"
+
+function check_cr() {
+  local OUTPUT="${1}"
+
+  if [[ "${OUTPUT}" != "true" && "${COUNT}" -le "${LIMIT}" ]]; then
+    sleep 1
+    ((++COUNT))
+  elif [[ "${COUNT}" -gt "${LIMIT}" ]]; then
+    >&2 echo "Limit exceeded."
+    exit 1
+  else
+    kubectl -n "${NAMESPACE}" get clickhouseinstallation "${RELEASE_NAME}" --ignore-not-found
+
+    exit 0
+  fi
+}
+
+COUNT=1
+while true; do
+  check_cr "$(kubectl -n "${NAMESPACE}" get clickhouseinstallation "${RELEASE_NAME}" --ignore-not-found -o yaml | yq ".status.status == \"Completed\"")"
+done
