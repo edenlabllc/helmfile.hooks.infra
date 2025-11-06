8452d2c2c48ae3a0bb167098a75b796bcf042020
Anton Novikov <a.novikov@edenlab.com.ua>
2025-11-06 18:10:55 +0100

    Refactor presync-create-postgres-user.sh to use yq and no console utils.

--- bin/presync-create-postgres-user.sh
+++ bin/presync-create-postgres-user.sh
@@ -13,11 +13,15 @@ PG_ENABLE_DEFAULT_USERS="${7:-false}"
 function create_default_user() {
   for DB in ${PG_DB_NAMES[*]}; do
     local DEFAULT_OWNER_USER="${DB}_owner_user"
-    if ! (kubectl --namespace "${NAMESPACE}" get secret | grep "^${DEFAULT_OWNER_USER//_/-}\.${PG_CLUSTER_NAME}" &> /dev/null); then #todo remove grep
-      kubectl --namespace "${PG_NAMESPACE}" patch "${PG_CRD_NAME}" "${PG_CLUSTER_NAME}" --type='merge' \
-        -p '{"spec":{"databases":{"'"${DB}"'":"'"${DEFAULT_OWNER_USER}"'"}}}'
-      kubectl --namespace "${PG_NAMESPACE}" patch "${PG_CRD_NAME}" "${PG_CLUSTER_NAME}" --type='merge' \
-        -p '{"spec":{"preparedDatabases":{"'"${DB}"'":{"defaultUsers":true,"schemas":{"public":{"defaultRoles":false}},"secretNamespace":"'"${NAMESPACE}"'"}}}}'
+    local SECRET_PREFIX="${DEFAULT_OWNER_USER//_/-}.${PG_CLUSTER_NAME}"
+
+    # Check secret existence without grep
+    if ! (kubectl --namespace "${NAMESPACE}" get secret --output yaml \
+      | yq --exit-status ".items[].metadata.name | select(startswith(\"${SECRET_PREFIX}\"))" > /dev/null); then
+      kubectl --namespace "${PG_NAMESPACE}" patch "${PG_CRD_NAME}" "${PG_CLUSTER_NAME}" --type=merge \
+        --patch '{"spec":{"databases":{"'"${DB}"'":"'"${DEFAULT_OWNER_USER}"'"}}}'
+      kubectl --namespace "${PG_NAMESPACE}" patch "${PG_CRD_NAME}" "${PG_CLUSTER_NAME}" --type=merge \
+        --patch '{"spec":{"preparedDatabases":{"'"${DB}"'":{"defaultUsers":true,"schemas":{"public":{"defaultRoles":false}},"secretNamespace":"'"${NAMESPACE}"'"}}}}'
     fi
   done
 
@@ -25,12 +29,16 @@ function create_default_user() {
 }
 
 function create_custom_user() {
-  if ! (kubectl --namespace "${NAMESPACE}" get secret | grep "^${NAMESPACE}\.${PG_DB_USERNAME//_/-}\.${PG_CLUSTER_NAME}" &> /dev/null); then #todo remove grep
-    kubectl --namespace "${PG_NAMESPACE}" patch "${PG_CRD_NAME}" "${PG_CLUSTER_NAME}" --type='merge' \
-      -p '{"spec":{"users":{"'"${NAMESPACE}"'.'"${PG_DB_USERNAME}"'":["createdb"]}}}'
+  local SECRET_PREFIX="${NAMESPACE}.${PG_DB_USERNAME//_/-}.${PG_CLUSTER_NAME}"
+
+  if ! (kubectl --namespace "${NAMESPACE}" get secret --output yaml \
+    | yq --exit-status ".items[].metadata.name | select(startswith(\"${SECRET_PREFIX}\"))" > /dev/null); then
+    kubectl --namespace "${PG_NAMESPACE}" patch "${PG_CRD_NAME}" "${PG_CLUSTER_NAME}" --type=merge \
+      --patch '{"spec":{"users":{"'"${NAMESPACE}"'.'"${PG_DB_USERNAME}"'":["createdb"]}}}'
+
     for DB in ${PG_DB_NAMES[*]}; do
-      kubectl --namespace "${PG_NAMESPACE}" patch "${PG_CRD_NAME}" "${PG_CLUSTER_NAME}" --type='merge' \
-        -p '{"spec":{"databases":{"'"${DB}"'":"'"${NAMESPACE}"'.'"${PG_DB_USERNAME}"'"}}}'
+      kubectl --namespace "${PG_NAMESPACE}" patch "${PG_CRD_NAME}" "${PG_CLUSTER_NAME}" --type=merge \
+        --patch '{"spec":{"databases":{"'"${DB}"'":"'"${NAMESPACE}"'.'"${PG_DB_USERNAME}"'"}}}'
     done
 
     sleep 5
