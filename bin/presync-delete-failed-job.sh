7a2a8138dcc888b5aa01b9b94ba42af16441b3d1
Anton Novikov <a.novikov@edenlab.com.ua>
2025-11-06 17:49:05 +0100

    Refactor presync-delete-failed-job.sh to use yq and no console utils.

--- bin/presync-delete-failed-job.sh
+++ bin/presync-delete-failed-job.sh
@@ -2,13 +2,34 @@
 
 set -e
 
-JOB_NAME="${1}"
+JOB_PREFIX="${1}"
 NAMESPACE="${2}"
 
-if (kubectl --namespace "${NAMESPACE}" get job | grep "${JOB_NAME}"); then #todo remove grep
-  JOB_ID=$(kubectl --namespace "${NAMESPACE}" get job | grep "${JOB_NAME}" | awk '{print $1}') #todo remove grep & awk
-  FAILED_COUNT=$(kubectl --namespace "${NAMESPACE}" get job "${JOB_ID}" --output json | yq --unwrapScalar '.status.failed')
-  if (( "${FAILED_COUNT}" > 0 )); then
-    kubectl --namespace "${NAMESPACE}" delete job "${JOB_ID}"
-  fi
+# Get all job names matching prefix
+JOB_NAMES="$(
+  kubectl --namespace "${NAMESPACE}" get job --output yaml \
+    | yq --unwrapScalar '
+        .items[]
+        | select(.metadata.name | startswith("'"${JOB_PREFIX}"'-"))
+        | .metadata.name
+      '
+)"
+
+if [[ -z "${JOB_NAMES}" ]]; then
+  echo "No jobs with prefix \"${JOB_PREFIX}-\" found in namespace ${NAMESPACE}. Skipped."
+  exit 0
 fi
+
+for JOB_NAME in ${JOB_NAMES}; do
+  FAILED_COUNT="$(
+    kubectl --namespace "${NAMESPACE}" get job "${JOB_NAME}" --output yaml \
+      | yq --unwrapScalar '.status.failed // 0'
+  )"
+
+  if (( FAILED_COUNT > 0 )); then
+    echo "Deleting failed job: ${JOB_NAME} (failed=${FAILED_COUNT})"
+    kubectl --namespace "${NAMESPACE}" delete job "${JOB_NAME}"
+  else
+    echo "Job ${JOB_NAME} is healthy. Skipped."
+  fi
+done
