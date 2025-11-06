eb0764e26eacd134bf9441efaa6ffadeca8db961
Anton Novikov <a.novikov@edenlab.com.ua>
2025-11-06 17:57:08 +0100

    Refactor postsync-label-resource.sh to use yq and no console utils.

--- bin/postsync-label-resource.sh
+++ bin/postsync-label-resource.sh
@@ -6,7 +6,7 @@ CURRENT_ENVIRONMENT="${1}"
 EXPECTED_ENVIRONMENT="${2}"
 K8S_NAMESPACE="${3}"
 K8S_RESOURCE_TYPE="${4}"
-K8S_RESOURCE_NAME="${5}"
+K8S_RESOURCE_NAME_OR_PREFIX="${5}"
 # get rest of arguments
 K8S_LABELS=("${@}")
 K8S_LABELS="${K8S_LABELS[@]:5}"
@@ -17,16 +17,25 @@ if [[ "${CURRENT_ENVIRONMENT}" != "${EXPECTED_ENVIRONMENT}" ]]; then
 fi
 
 if [[ "${K8S_RESOURCE_TYPE}" == "pod" ]]; then
-  echo "Annotating all pods with prefix ${K8S_RESOURCE_NAME} in namespace ${K8S_NAMESPACE}"
-  # todo remove grep
-  PODS=$(kubectl --namespace "${K8S_NAMESPACE}" get pod --no-headers --output custom-columns=":metadata.name" | grep "^${K8S_RESOURCE_NAME}")
+  echo "Annotating all pods with prefix ${K8S_RESOURCE_NAME_OR_PREFIX}- in namespace ${K8S_NAMESPACE}"
+
+  PODS="$(
+    kubectl --namespace "${K8S_NAMESPACE}" get pod --output yaml \
+      | yq --unwrapScalar '
+          .items[]
+          | select(.metadata.name | startswith("'"${K8S_RESOURCE_NAME_OR_PREFIX}"'-"))
+          | .metadata.name
+        '
+  )"
+
   if [[ -z "${PODS}" ]]; then
-    echo "No pods found with prefix ${K8S_RESOURCE_NAME}"
+    echo "No pods found with prefix ${K8S_RESOURCE_NAME_OR_PREFIX}-"
     exit 0
   fi
+
   for POD in ${PODS}; do
     kubectl --namespace "${K8S_NAMESPACE}" label --overwrite pod "${POD}" ${K8S_LABELS}
   done
 else
-  kubectl --namespace "${K8S_NAMESPACE}" label --overwrite "${K8S_RESOURCE_TYPE}" "${K8S_RESOURCE_NAME}" ${K8S_LABELS}
+  kubectl --namespace "${K8S_NAMESPACE}" label --overwrite "${K8S_RESOURCE_TYPE}" "${K8S_RESOURCE_NAME_OR_PREFIX}" ${K8S_LABELS}
 fi
