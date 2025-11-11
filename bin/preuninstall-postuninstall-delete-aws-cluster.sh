cc43fb58891834c659f6c44a60c14657c1bb77ae
Anton Novikov <a.novikov@edenlab.com.ua>
2025-11-11 17:54:45 +0100

    Remove all 9 hook files (now stored in cluster-deps): postsync-wait-aws-cluster-ready.sh postsync-wait-aws-iam-provision-ready.sh postsync-wait-azure-cluster-ready.sh postsync-wait-gcp-cluster-ready.sh postsync-wait-onprem-cluster-ready.sh preuninstall-postuninstall-delete-aws-cluster.sh preuninstall-postuninstall-delete-azure-cluster.sh preuninstall-postuninstall-delete-gcp-cluster.sh preuninstall-postuninstall-delete-onprem-cluster.sh

--- bin/preuninstall-postuninstall-delete-aws-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-NAMESPACE="${1}"
-RELEASE_NAME="${2}"
-WAIT_FOR_CLUSTER_DELETION="${3:-false}"
-
-kubectl --namespace "${NAMESPACE}" delete cluster "${RELEASE_NAME}" --wait="${WAIT_FOR_CLUSTER_DELETION}"
