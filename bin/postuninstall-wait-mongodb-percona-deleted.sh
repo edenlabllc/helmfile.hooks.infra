efcf2f7f7b95ce59e3473c4e0ab83dc866630a20
Vadim <vadim_bogdanovsky@yahoo.com>
2025-11-19 14:11:18 +0200

    add hooks for mongodb-percona release

---
+++ bin/postuninstall-wait-mongodb-percona-deleted.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly NAMESPACE="${1}"
+readonly RELEASE_NAME="${2}"
+readonly LIMIT="${3:-180}"
+
+function check_psmdb() {
+  local OUTPUT="${1}"
+
+  if [[ "${OUTPUT}" != "true" && "${COUNT}" -le "${LIMIT}" ]]; then
+    sleep 1
+    (( ++COUNT ))
+  elif [[ "${COUNT}" -gt "${LIMIT}" ]]; then
+    >&2 echo "$(basename "${0}"): Wait timeout exceeded."
+    exit 1
+  else
+    echo
+    kubectl --namespace "${NAMESPACE}" get perconaservermongodb "${RELEASE_NAME}" --ignore-not-found
+    exit 0
+  fi
+}
+
+COUNT=1
+while true; do
+  check_psmdb "$(kubectl --namespace "${NAMESPACE}" get perconaservermongodb "${RELEASE_NAME}" --ignore-not-found --output yaml | yq "length == 0")"
+done
