4f6b0fcaea1e7604282c9518ca0d052d47a517a5
Vadim <vadim_bogdanovsky@yahoo.com>
2026-04-30 15:04:51 +0300

    additional selectors for presync-upgrade-crds hook

--- bin/presync-upgrade-crds.sh
+++ bin/presync-upgrade-crds.sh
@@ -4,12 +4,12 @@ set -e
 
 readonly CHART_FULL_NAME="${1}"
 readonly CHART_VERSION="${2}"
-readonly ADD_SELECTORS="${3}"
+readonly ADDITIONAL_SELECTORS="${3}"
 
 SELECTORS='.kind == "CustomResourceDefinition"'
 
-if [[ -n "${ADD_SELECTORS}" ]]; then
-  SELECTORS="${SELECTORS} and ${ADD_SELECTORS}"
+if [[ -n "${ADDITIONAL_SELECTORS}" ]]; then
+  SELECTORS="${SELECTORS} and ${ADDITIONAL_SELECTORS}"
 fi
 
 helm template "${CHART_FULL_NAME}" --version "${CHART_VERSION}" --include-crds | yq "select(${SELECTORS})" | kubectl apply --filename -
