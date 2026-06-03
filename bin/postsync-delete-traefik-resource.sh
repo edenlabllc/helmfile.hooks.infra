7b479220ccf2dc5f8bbdf1a1c69b12721cf99f8f
Vadim <vadim_bogdanovsky@yahoo.com>
2026-06-03 10:29:39 +0300

    add postsync hook for delete deprecated traefik resources

--- bin/postsync-delete-traefik-resource.sh
+++ bin/postsync-delete-traefik-resource.sh
@@ -6,7 +6,7 @@ readonly NAMESPACE="${1}"
 readonly RELEASE_NAME="${2}"
 
 for CRD in ingressroutes.traefik.containo.us middlewares.traefik.containo.us; do
-  if (kubectl get crd "${CRD}" >/dev/null 2>&1); then
+  if (kubectl get customresourcedefinitions "${CRD}" >/dev/null 2>&1); then
     kubectl --namespace "${NAMESPACE}" delete "${CRD}" \
       --selector "app.kubernetes.io/instance=${RELEASE_NAME}" \
       --ignore-not-found=true
